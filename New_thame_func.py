956359b3ea0e68ea22f28fb4a04bdc728ca46dd7
DarkyProvide_Developer <48835048+DarkyProvide@users.noreply.github.com>
2019-12-03 15:52:25 +0300

    'Add2'

---
+++ New_thame_func.py
@@ -0,0 +1,20 @@
+def new_func_search_text_in_progress(s1, s2):
+
+    result = 0
+    s2_to = s2
+
+    for i in s1:
+        if i in s2_to:
+            s2_to = s2_to.replace(char, '', 1)
+            result += 1
+
+    r1 = int(result / len(s1) * 100)
+    r2 = int(result / len(s2) * 100)
+
+    return '{}%'.format(r1 if r1 < r2 else r2)
+
+s1 = 'Hellow'
+
+while True:
+    s2 = input('_')
+    new_func_search_text_in_progress(s1, s2)
